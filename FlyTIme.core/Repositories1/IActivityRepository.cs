562c691722ecef1d8535a5c77b8f95bf96c567c0
oussama okbani <oussama.okbani2013@gmail.com>
2024-05-22 02:24:50 +0200

    Ajout des nouveaux models, repos et services dans la couche Core et l'ajout de la couche service

---
+++ FlyTIme.core/Repositories1/IActivityRepository.cs
@@ -0,0 +1,12 @@
+﻿using FlyTime.Core.Models1;
+
+namespace FlyTime.Core.Repositories
+{
+    public interface IActivityRepository : IRepository<Activity>
+    {
+        public Task<Activity> GetByEndTime(DateTime endTime);
+        public Task<Activity> GetByStartTime(DateTime startTime);
+        Task<IEnumerable<Activity>> GetActivityByVol(Vol vol);
+
+    }
+}
