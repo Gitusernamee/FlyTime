ba1109a27c1c66df2d001d998ab028cb3ea3cacd
mohammed <Mohammed.Sanbouli@emsi-edu.ma>
2024-05-22 04:22:29 +0100

    deplacement de IUnitOfwork

--- FlyTime.Service/Services/ActivityService.cs
+++ FlyTime.Service/Services/ActivityService.cs
@@ -1,68 +1,142 @@
-﻿using FlyTime.core.Models1;
+﻿// using FlyTime.core.Models1;
+// using FlyTime.core.Repositories1;
+// using FlyTime.core.Services1;
+
+// namespace FlyTime.Service.Services
+// {
+//     public class ActivityService : IActivityService
+//     {
+//         private readonly IActivityRepository activityRepository;
+
+//         public Task<Activity> CreateActivity(Activity activity)
+//         {
+//             return activityRepository.AddAsync(activity);
+//         }
+
+//         public void DeleteActivity(Activity activity)
+//         {
+//             activityRepository.Remove(activity);
+//         }
+
+//         public Task<Activity> GetActivityByEndTime(DateTime endTime)
+//         {
+//             return activityRepository.GetByEndTime(endTime);
+//         }
+
+//         public ValueTask<Activity> GetActivityById(int id)
+//         {
+//             return activityRepository.GetByIdAsync(id);
+//         }
+
+//         public Task<Activity> GetActivityByStartTime(DateTime startTime)
+//         {
+//             return activityRepository.GetByStartTime(startTime);
+//         }
+
+//         //***********************TODO********************************
+//         public Task<IEnumerable<Activity>> GetActivityByVol(Vol vol)
+//         {
+//             throw new NotImplementedException();
+//         }
+//         //***********************************************************
+
+//         public Task<Activity> UpdateActivity(Activity activity, int id)
+//         {
+//             ValueTask<Activity> checkActivity = activityRepository.GetByIdAsync(id);
+
+//             Activity activityFromDb;
+
+//             if (checkActivity.IsCompletedSuccessfully)
+//             {
+//                 activityFromDb = checkActivity.Result;
+
+//                 activityFromDb.FromDestination = activity.FromDestination;
+//                 activityFromDb.ToDestination = activity.ToDestination;
+//                 activityFromDb.StartTime = activity.StartTime;
+//                 activityFromDb.EndTime = activity.EndTime;
+
+//                 return activityRepository.AddAsync(activityFromDb);
+//             }
+//             else
+//             {
+//                 throw new Exception("Operation Canceled or Activity not found");
+//             }
+
+//             return null;
+//         }
+//     }
+// }
+using FlyTime.core.Models1;
 using FlyTime.core.Repositories1;
 using FlyTime.core.Services1;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FlyTime.Service.Services
 {
     public class ActivityService : IActivityService
     {
-        private readonly IActivityRepository activityRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public Task<Activity> CreateActivity(Activity activity)
+        public ActivityService(IUnitOfWork unitOfWork)
         {
-            return activityRepository.AddAsync(activity);
+            _unitOfWork = unitOfWork;
         }
 
-        public void DeleteActivity(Activity activity)
+        public async Task<Activity> CreateActivity(Activity activity)
         {
-            activityRepository.Remove(activity);
+            await _unitOfWork.ActivityRepository.AddAsync(activity);
+            await _unitOfWork.CommitAsync();
+            return activity;
+        }
+
+        public async Task DeleteActivity(Activity activity)
+        {
+            _unitOfWork.ActivityRepository.Remove(activity);
+            await _unitOfWork.CommitAsync();
         }
 
         public Task<Activity> GetActivityByEndTime(DateTime endTime)
         {
-            return activityRepository.GetByEndTime(endTime);
+            return _unitOfWork.ActivityRepository.GetByEndTime(endTime);
         }
 
-        public ValueTask<Activity> GetActivityById(int id)
+        public Task<Activity> GetActivityById(int id)
         {
-            return activityRepository.GetByIdAsync(id);
+            return _unitOfWork.ActivityRepository.GetByIdAsync(id);
         }
 
         public Task<Activity> GetActivityByStartTime(DateTime startTime)
         {
-            return activityRepository.GetByStartTime(startTime);
+            return _unitOfWork.ActivityRepository.GetByStartTime(startTime);
         }
 
-        //***********************TODO********************************
+        // Méthode pour récupérer les activités par vol
         public Task<IEnumerable<Activity>> GetActivityByVol(Vol vol)
         {
-            throw new NotImplementedException();
+            return _unitOfWork.ActivityRepository.GetByVol(vol);
         }
-        //***********************************************************
 
-        public Task<Activity> UpdateActivity(Activity activity, int id)
+        public async Task<Activity> UpdateActivity(Activity activity, int id)
         {
-            ValueTask<Activity> checkActivity = activityRepository.GetByIdAsync(id);
+            var existingActivity = await _unitOfWork.ActivityRepository.GetByIdAsync(id);
 
-            Activity activityFromDb;
-
-            if (checkActivity.IsCompletedSuccessfully)
+            if (existingActivity == null)
             {
-                activityFromDb = checkActivity.Result;
-
-                activityFromDb.FromDestination = activity.FromDestination;
-                activityFromDb.ToDestination = activity.ToDestination;
-                activityFromDb.StartTime = activity.StartTime;
-                activityFromDb.EndTime = activity.EndTime;
-
-                return activityRepository.AddAsync(activityFromDb);
-            }
-            else
-            {
-                throw new Exception("Operation Canceled or Activity not found");
+                throw new Exception("Activity not found");
             }
 
-            return null;
+            // Mettre à jour les propriétés de l'activité existante avec les nouvelles valeurs
+            existingActivity.FromDestination = activity.FromDestination;
+            existingActivity.ToDestination = activity.ToDestination;
+            existingActivity.StartTime = activity.StartTime;
+            existingActivity.EndTime = activity.EndTime;
+
+            _unitOfWork.ActivityRepository.Update(existingActivity);
+            await _unitOfWork.CommitAsync();
+
+            return existingActivity;
         }
     }
 }
